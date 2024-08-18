94eb86dc8a0ccd3676155eb2f125e81ba57f33ec
Rafael Barbosa <rafael.barbosa51@gmail.com>
2024-08-18 12:07:24 -0500

    functional

---
+++ main.js
@@ -0,0 +1,11 @@
+let active = false;
+document.addEventListener('click', () => active = !active)
+document.addEventListener('pointermove', e => {
+  if (active) {
+    const body = document.querySelector('body')
+    const circle = document.createElement('span')
+    circle.style.left = e.offsetX + 'px'
+    circle.style.top = e.offsetY + 'px'
+    body.appendChild(circle)
+  }
+})
